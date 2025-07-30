7f7e4f98556a51358b6f462700f06a98d1773c49
Daniel Wilson <dwilson@seerai.space>
2025-07-30 10:07:01 -0600

    fixes pagination and datatype bugs, update example script

--- src/main/scala/ai/seer/geodesic/DatasetInfo.scala
+++ src/main/scala/ai/seer/geodesic/DatasetInfo.scala
@@ -1,6 +1,7 @@
 package ai.seer.geodesic
 
 import play.api.libs.json._
+import play.api.libs.json.JsonNaming.SnakeCase
 
 case class FieldDef(
     title: String,
@@ -8,13 +9,24 @@ case class FieldDef(
     format: Option[String] = None
 )
 
+case class ProviderConfig(
+    providerName: String,
+    url: String,
+    maxPageSize: Int = 10000
+)
+
 case class DatasetInfo(
     name: String,
     alias: String,
+    config: Option[ProviderConfig] = None,
     fields: Map[String, Map[String, FieldDef]] = Map.empty
 )
 
 object DatasetInfo {
+  implicit val jsonConfig = JsonConfiguration(
+    SnakeCase
+  )
   implicit val fieldDefReads = Json.reads[FieldDef]
+  implicit val providerConfigReads = Json.reads[ProviderConfig]
   implicit val datasetInfoReads = Json.reads[DatasetInfo]
 }
